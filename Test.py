c5b4ec7e1d933fa5b241b21ed1f21934c251f75d
raunak <raunakbiswas05@gmail.com>
2024-12-23 15:30:24 +0530

    Using numpy array and cv2 to make images

--- Test.py
+++ Test.py
@@ -1,6 +1,26 @@
 import cv2
+import numpy
 
-array = cv2.imread("image.png")
+# array = cv2.imread("image.png")
+#
+# print(array.shape)
+# print(array)
 
-print(array.shape)
-print(array)
+#-------------------------------- making images-----------------------------------
+
+img_data = numpy.array([[[255, 0, 0],
+  [255, 255, 255],
+  [255, 255, 255],
+  [187, 41, 160]],
+
+ [[255, 255, 255],
+  [255, 255, 255],
+  [255, 255, 255],
+  [255, 255, 255]],
+
+ [[255, 255, 255],
+  [0, 0, 0],
+  [ 47, 255, 173],
+  [255, 255, 255]]])
+
+cv2.imwrite("img2.png", img_data)
