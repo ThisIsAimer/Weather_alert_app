da3063e6c6cfa87da30c1225a8aad0d165133d38
raunak <raunakbiswas05@gmail.com>
2024-12-30 12:43:22 +0530

    video capture through python

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print("hello")
+import cv2
+import time
+
+video = cv2.VideoCapture(1) #captures main camera with 0 and external and virtual camera with 1
+
+
+while True:
+    check,frame = video.read()
+    time.sleep(0)
+    cv2.imshow("My video" , frame)
+
+    key = cv2.waitKey(1)
+
+    if key == ord("q"):
+        break
+
+video.release()
