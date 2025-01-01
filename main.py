e08769fd8e2df0a8398456c8290bff96d15f92ff
raunak <raunakbiswas05@gmail.com>
2025-01-01 15:20:32 +0530

    Detecting moving objects with python

--- main.py
+++ main.py
@@ -17,9 +17,22 @@ while True:
     if first_frame is None:
         first_frame = grey_frame
 
-    delta_frame = cv2.absdiff( first_frame , grey_frame)
+    delta_frame = cv2.absdiff( first_frame , grey_frame) # show changes in frame
 
-    cv2.imshow("My video", delta_frame)
+    thresh_frame = cv2.threshold(delta_frame,45,255,cv2.THRESH_BINARY)[1] # makes difference bigger
+
+    dil_frame = cv2.dilate(thresh_frame,None,iterations=2)  # makes the thresh bigger
+
+    contours, check = cv2.findContours(dil_frame,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE) # detects outer shape of changed areas
+
+    for contour in contours:
+        if cv2.contourArea(contour) < 10000:
+            continue
+
+        x,y,w,h = cv2.boundingRect(contour)
+        cv2.rectangle(frame,(x,y),(x+w,y+h), (0,225,0))
+
+    cv2.imshow("My video", frame)
 
     key = cv2.waitKey(1)
 
