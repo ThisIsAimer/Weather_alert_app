fed5d2d076c7ec270de81a0d921d44ccbea37860
raunak <raunakbiswas05@gmail.com>
2025-01-01 11:12:03 +0530

    Capturing delta frames and detecting changes in frame

--- main.py
+++ main.py
@@ -3,11 +3,23 @@ import time
 
 video = cv2.VideoCapture(1) #captures main camera with 0 and external and virtual camera with 1
 
+first_frame = None
 
 while True:
     check,frame = video.read()
+
+    grey_frame = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+
+    grey_gau = cv2.GaussianBlur(grey_frame,(21,21),0)
     time.sleep(0)
-    cv2.imshow("My video" , frame)
+
+
+    if first_frame is None:
+        first_frame = grey_frame
+
+    delta_frame = cv2.absdiff( first_frame , grey_frame)
+
+    cv2.imshow("My video", delta_frame)
 
     key = cv2.waitKey(1)
 
